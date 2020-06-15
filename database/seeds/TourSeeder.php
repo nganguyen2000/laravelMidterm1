1f032f1824d40efd086a50331dcb98a72c5d6d4b
Nguyen Thi Nga <nga.nguyen21@pn.com.vn>
2020-06-15 09:33:53 +0700

    Use seed and faker to insert 10 record

--- database/seeds/TourSeeder.php
+++ database/seeds/TourSeeder.php
@@ -11,15 +11,19 @@ class TourSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('tours')->insert([
-            'name' => 'Tour mien Trung - Da Nang - Ba Na -Hoi An',
-            'image' => 'https://travel.com.vn/images/destination/Large/tf_160129_dc_150910_HoiAn.jpg',
-            'typetour' =>'Tu Ho Chi Minh',
-            'Schedule' =>'4 ngay 3 dem',
-            'depart'=>'12/5/2020',
-            'number'=>'9',
-            'price'=>9000000,
+        $faker = \Faker\Factory::create();
+        $i=0;
+        for($i== 0; $i<10; $i++) {
+            DB::table('tours')->insert([
+               'name' => $faker->sentence,
+                'image' => 'https://travel.com.vn/images/destination/Large/tf_160129_dc_150910_HoiAn.jpg',
+                'typetour' =>'Tu Ho Chi Minh',
+                'Schedule' =>'4 ngay 3 dem',
+                'depart'=>'12/5/2020',
+                'number'=>'9',
+                'price'=>9000000,
 
-        ]);
+            ]);
+        }
     }
 }
